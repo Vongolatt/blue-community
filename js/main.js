2fd91c5da492f98f5b79cc4fb452c01da8542bd5
Vongolatt <1085280247@qq.com>
2017-03-13 11:36:44 +0800

    商品详情静态页

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 document.documentElement.style.fontSize =document.documentElement.clientWidth/6.4+'px';
-document.body.addEventListener('touchmove', function () { }); 
+document.body.addEventListener('touchmove', function () { });
+// $(function () {
+// 	//输入框获取焦点时隐藏底部栏
+// 	$('input').focus(function(event) {
+// 		$('footer').find('nav').css('display', 'none');
+// 	})
+// 	.blur(function(event) {
+// 		$('footer').find('nav').css('display', 'block');
+// 	});
+// })
