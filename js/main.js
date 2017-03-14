bf4e13084a028f4b1d620b3b0d5bdb26450c1e77
Vongolatt <1085280247@qq.com>
2017-03-14 18:29:59 +0800

    添加日历控件

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ document.body.addEventListener('touchmove', function () { });
 // 		$('footer').find('nav').css('display', 'block');
 // 	});
 // })
+
+$(function(){
+	var year = (new Date()).getFullYear()+1;
+	$('.calendar').calendar({
+		limitRange:[['today',year+'0101']],
+		isRange:true,
+		'onSelect':function(range){
+			var start_time = range[0]['year']+'-'+range[0]['month']+'-'+range[0]['day'];
+			var end_time = range[1]['year']+'-'+range[1]['month']+'-'+range[1]['day'];
+			$('.date').children('span').first().html(start_time);
+			$('.date').children('span').last().html(end_time);
+			$('.calendar').slideUp('slow');
+		}
+	});
+	$('.date').click(function(event) {
+		$('.calendar').slideToggle('slow');
+	});
+
+})
+
